7b263c7f358989dad9c69ba92d8f17854780158c
Maxim <mamaama@mail.ru>
2026-01-19 21:16:39 +1000

    Add divide function

--- calculator.py
+++ calculator.py
@@ -4,4 +4,9 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-print(Calculator v1.0)
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "Error: Division by zero"
+
+print("Calculator v1.0")
